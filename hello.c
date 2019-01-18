e912a4f019764b68339d575830dfc959c50b5a12
maxkhilou <46799838+maxkhilou@users.noreply.github.com>
2019-01-17 19:11:25 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,8 +1,18 @@
+/**
+ * Author: Max Khilou
+ * Date: 01/17.2019
+ *
+ * A simple hello world program in C
+ *
+ */
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[]) {
+  
   printf("Max Khilou\n");
   printf("Computer Science\n");
+  
   printf("Andrew\n");
   printf("Computer Science and Classical Languages\n");
 
