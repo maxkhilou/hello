5fe44f49af023a745bf3720effbb66cb4bb984b4
Andrew Malesker <drewm2000@icloud.com>
2019-01-17 18:56:13 -0600

    Update hello.c
    Added my name and major

--- hello.c
+++ hello.c
@@ -3,6 +3,9 @@
 int main(int argc, char const *argv[]) {
   printf("Max Khilou\n");
   printf("Computer Science\n");
-  
+  printf("Andrew\n");
+  printf("Computer Science and Classical Languages\n");
+
+
   return 0;
 }
